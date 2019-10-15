57f8ffc6659cef43c497bd30525fd2e797463dbf
Abby Berkers <abbyberkers@gmail.com>
2019-10-15 08:09:17 +0200

    Fix ui

--- kotlin-app/app/src/main/java/com/abby/grocerylist/GroceryViewHolder.kt
+++ kotlin-app/app/src/main/java/com/abby/grocerylist/GroceryViewHolder.kt
@@ -2,8 +2,6 @@ package com.abby.grocerylist
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.grocery_list_item.view.*
 
@@ -17,8 +15,8 @@ class GroceryViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
      * Bind a Grocery to its view.
      */
     fun bind(grocery: Grocery) = with(itemView) {
-        this.findViewById<TextView>(R.id.grocery_item).text = grocery.item
-        this.findViewById<CheckBox>(R.id.grocery_done).isChecked = grocery.done
+        grocery_done.isChecked = grocery.done
+        grocery_done.text = grocery.item
         // Set listener on the checkbox to update the value in firebase.
         grocery_done.setOnCheckedChangeListener { _, newDone -> grocery.update(newDone) }
     }
--- kotlin-app/app/src/main/java/com/abby/grocerylist/SwipeToDeleteCallback.kt
+++ kotlin-app/app/src/main/java/com/abby/grocerylist/SwipeToDeleteCallback.kt
@@ -4,17 +4,21 @@ import android.content.Context
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 
-class SwipeToDeleteCallback(val context: Context, private val adapter: GroceriesAdapter) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+class SwipeToDeleteCallback(val context: Context,
+                            private val adapter: GroceriesAdapter
+) :
+    ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        val position = viewHolder.adapterPosition
+        adapter.groceries[position].delete()
+    }
+
     override fun onMove(
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder,
         target: RecyclerView.ViewHolder
     ): Boolean {
         return false
-    }
-
-    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-        val position = viewHolder.adapterPosition
-        adapter.groceries[position].delete()
     }
 }
